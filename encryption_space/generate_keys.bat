7cfb5b8bba49b75cfb4a2021b88afc21f8c33fe4
vsemenyakin <volodymyr.semenyakin@fracturedbyte.com>
2023-04-03 00:09:27 +0300

    - Added "Rem" before seporator inside encryption scripts

--- encryption_space/generate_keys.bat
+++ encryption_space/generate_keys.bat
@@ -8,5 +8,5 @@ if exist %keys_folder_name% (
   %openssl_path% genrsa -aes128 -out %keys_folder_name%\private.pem 1024
   %openssl_path% rsa -in %keys_folder_name%\private.pem -pubout > %keys_folder_name%\public.pem
 )
-----------------------------------------------------------------------------------------
+Rem ----------------------------------------------------------------------------------------
 set /p DUMMY=Press ENTER to close...
