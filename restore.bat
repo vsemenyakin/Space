4f4bcb305418b0de17613cd0f44efd5f5fe38b2e
vsemenyakin <volodymyr.semenyakin@fracturedbyte.com>
2023-04-05 11:00:36 +0300

    - Reworked git-base restore script for new work flow: automated forming of repo from manual got copies of repos from hub users

--- restore.bat
+++
@@ -1,9 +0,0 @@
-call ./_/vars.bat
-Rem ----------------------------------------------------------------------------------------
-call ./_/setup_git_local.bat
-Rem ----------------------------------------------------------------------------------------
-git -C %data_base_folder_name% remote add restore-remote %restore_repository_path%
-git -C %data_base_folder_name% checkout -b restore-branch/%user_name%
-git -C %data_base_folder_name% push restore-remote restore-branch/%user_name%
-Rem ----------------------------------------------------------------------------------------
-set /p DUMMY=Press ENTER to close...
---
+++ restore_space/restore.bat
@@ -0,0 +1,26 @@
+Rem ---| Variables |---
+set restore_folder_name=.
+set local_restore_repo_folder_name=__restore_repo__
+set restore_repository_path=https://github.com/vsemenyakin/SilenceWarlord_Resotre.git
+Rem ----------------------------------------------------------------------------------------
+call ./_/setup_git_local.bat
+Rem ----------------------------------------------------------------------------------------
+setlocal
+Rem ----------------------------------------------------------------------------------------
+pushd %restore_folder_name%
+for /D %%A in (*) do (
+  git -C %%A remote add restore-remote %restore_repository_path%
+  git -C %%A push restore-remote master:restore/%%A
+  git -C %%A remote remove restore-remote
+)
+popd
+Rem ----------------------------------------------------------------------------------------
+if exist %local_restore_repo_folder_name% (
+  rd /S /Q %local_restore_repo_folder_name%
+)
+mkdir %local_restore_repo_folder_name%
+git -C ./%local_restore_repo_folder_name% clone %restore_repository_path%
+Rem ----------------------------------------------------------------------------------------
+endlocal
+Rem ----------------------------------------------------------------------------------------
+set /p DUMMY=Press ENTER to close...
